78584e4909d5770c7ae15c52a062cad8cc755153
adeshina bhadmus <adeshinabhadmus5@gmail.com>
2025-07-28 13:02:49 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    extend: {
+      colors:{
+        Green500:" hsl(158, 36%, 37%)",
+        Green700: "hsl(158, 42%, 18%)",
+        Black: "hsl(212, 21%, 14%)",
+        Grey: "hsl(228, 12%, 48%)",
+        Cream: "hsl(30, 38%, 92%)",
+        White: "hsl(0, 0%, 100%)"
+      },
+      fontFamily: {
+        fraunces: ['Fraunces', 'serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
